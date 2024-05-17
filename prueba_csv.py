b9aaa1ad5f3cd7ea0ed851c291981384047d1a12
Dars2000 <128153675+Dars2000@users.noreply.github.com>
2024-05-17 13:05:40 -0400

    Update prueba_csv.py

--- prueba_csv.py
+++ prueba_csv.py
@@ -7,7 +7,6 @@ import requests
 
 # Función para enviar datos a la API
 def enviar_a_api(datos):
-    # Esta función debe enviar los datos a la API
     print("Datos enviados a la API:", datos)
 
 # Función para aplicar la lógica a cada valor de detección
@@ -23,29 +22,49 @@ def aplicar_logica(valor):
     elif valor == 0:
         return 0
 
+# Función para actualizar una línea específica en el archivo CSV
+def actualizar_linea_csv(linea_index, datos):
+    with open('resultados.csv', 'r', newline='') as csvfile:
+        reader = list(csv.reader(csvfile))
+
+    # Asegurar que la longitud de reader sea al menos linea_index + 1
+    while len(reader) <= linea_index:
+        reader.append([])
+
+    # Actualizar la línea correspondiente
+    reader[linea_index] = datos
+
+    with open('resultados.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(reader)
+
 # Función para actualizar las detecciones y enviar los datos a la API
-def actualizar_detecciones(linea):
+def actualizar_detecciones(linea, linea_index):
     if len(linea) == 2:
         deteccion1, tiempo = map(int, linea)
         deteccion1 = aplicar_logica(deteccion1)
         print(f"deteccion1: {deteccion1}")
-        for _ in range(tiempo):  # Actualizar cada segundo durante 15 segundos
+        print(f"tiempo asignado: {tiempo}")
+        for _ in range(tiempo):
             datos_actuales = {'deteccion1': deteccion1}
             enviar_a_api(datos_actuales)
-            r = requests.post(f"http://localhost:5030/escribir_deteccion1?s={deteccion1}")
+            requests.post(f"http://192.168.95.190:5030/escribir_deteccion1?s={deteccion1}")
             time.sleep(1)
+        actualizar_linea_csv(linea_index, [deteccion1])
     elif len(linea) == 3:
         deteccion1, deteccion2, tiempo = map(int, linea)
         deteccion1 = aplicar_logica(deteccion1)
         deteccion2 = aplicar_logica(deteccion2)
         print(f"deteccion1: {deteccion1}")
         print(f"deteccion2: {deteccion2}")
-        for _ in range(tiempo):  # Actualizar cada segundo durante 15 segundos
+        print(f"tiempo asignado: {tiempo}")
+        for _ in range(tiempo):
             datos_actuales = {'deteccion1': deteccion1, 'deteccion2': deteccion2}
             enviar_a_api(datos_actuales)
-            r = requests.post(f"http://localhost:5030/escribir_deteccion1?s={deteccion1}")
-            r = requests.post(f"http://localhost:5030/escribir_deteccion2?s={deteccion2}")
+            requests.post(f"http://192.168.95.190:5030/escribir_deteccion1?s={deteccion1}")
+            requests.post(f"http://192.168.95.190:5030/escribir_deteccion2?s={deteccion2}")
             time.sleep(1)
+        actualizar_linea_csv(linea_index, [deteccion1, deteccion2])
     elif len(linea) == 4:
         deteccion1, deteccion2, deteccion3, tiempo = map(int, linea)
         deteccion1 = aplicar_logica(deteccion1)
@@ -54,13 +73,15 @@ def actualizar_detecciones(linea):
         print(f"deteccion1: {deteccion1}")
         print(f"deteccion2: {deteccion2}")
         print(f"deteccion3: {deteccion3}")
-        for _ in range(tiempo):  # Actualizar cada segundo durante 15 segundos
+        print(f"tiempo asignado: {tiempo}")
+        for _ in range(tiempo):
             datos_actuales = {'deteccion1': deteccion1, 'deteccion2': deteccion2, 'deteccion3': deteccion3}
             enviar_a_api(datos_actuales)
-            r = requests.post(f"http://localhost:5030/escribir_deteccion1?s={deteccion1}")
-            r = requests.post(f"http://localhost:5030/escribir_deteccion2?s={deteccion2}")
-            r = requests.post(f"http://localhost:5030/escribir_deteccion3?s={deteccion3}")
+            requests.post(f"http://192.168.95.190:5030/escribir_deteccion1?s={deteccion1}")
+            requests.post(f"http://192.168.95.190:5030/escribir_deteccion2?s={deteccion2}")
+            requests.post(f"http://192.168.95.190:5030/escribir_deteccion3?s={deteccion3}")
             time.sleep(1)
+        actualizar_linea_csv(linea_index, [deteccion1, deteccion2, deteccion3])
     elif len(linea) == 5:
         deteccion1, deteccion2, deteccion3, deteccion4, tiempo = map(int, linea)
         deteccion1 = aplicar_logica(deteccion1)
@@ -71,40 +92,39 @@ def actualizar_detecciones(linea):
         print(f"deteccion2: {deteccion2}")
         print(f"deteccion3: {deteccion3}")
         print(f"deteccion4: {deteccion4}")
-        for _ in range(tiempo):  # Actualizar cada segundo durante 15 segundos
+        print(f"tiempo asignado: {tiempo}")
+        for _ in range(tiempo):
             datos_actuales = {'deteccion1': deteccion1, 'deteccion2': deteccion2, 'deteccion3': deteccion3, 'deteccion4': deteccion4}
             enviar_a_api(datos_actuales)
-            r = requests.post(f"http://localhost:5030/escribir_deteccion1?s={deteccion1}")
-            r = requests.post(f"http://localhost:5030/escribir_deteccion2?s={deteccion2}")
-            r = requests.post(f"http://localhost:5030/escribir_deteccion3?s={deteccion3}")
-            r = requests.post(f"http://localhost:5030/escribir_deteccion4?s={deteccion4}")
+            requests.post(f"http://192.168.95.190:5030/escribir_deteccion1?s={deteccion1}")
+            requests.post(f"http://192.168.95.190:5030/escribir_deteccion2?s={deteccion2}")
+            requests.post(f"http://192.168.95.190:5030/escribir_deteccion3?s={deteccion3}")
+            requests.post(f"http://192.168.95.190:5030/escribir_deteccion4?s={deteccion4}")
             time.sleep(1)
+        actualizar_linea_csv(linea_index, [deteccion1, deteccion2, deteccion3, deteccion4])
     else:
         print("La línea no tiene un número válido de valores:", linea)
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
-        # Cuando el archivo CSV se modifica, se ejecuta esta función
         with open('datos.csv', newline='') as csvfile:
             reader = csv.reader(csvfile)
-            for linea in reader:
-                actualizar_detecciones(linea)
+            for index, linea in enumerate(reader):
+                actualizar_detecciones(linea, index)
 
-# Función para iniciar la vigilancia del archivo CSV
 def iniciar_vigilancia():
     event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler, path='.', recursive=False)
     observer.start()
 
-# Función para iniciar el hilo de actualización de detecciones
 def iniciar_actualizacion():
-    while True:  # Bucle infinito para mantener la lectura continua del archivo CSV
+    while True:
         with open('datos.csv', newline='') as csvfile:
             reader = csv.reader(csvfile)
-            for linea in reader:
-                actualizar_detecciones(linea)
-        time.sleep(1)  # Espera un segundo antes de comenzar a leer el archivo nuevamente
+            for index, linea in enumerate(reader):
+                actualizar_detecciones(linea, index)
+        time.sleep(1)
 
 # Iniciar el proceso de actualización
 iniciar_actualizacion()
